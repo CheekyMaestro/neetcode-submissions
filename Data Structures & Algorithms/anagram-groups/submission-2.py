506eadd4a2e7c240cf600d832d6fcf20d4dcae0b
CheekyMaestro <arifathan@gmail.com>
2026-05-19 10:25:43 +0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        histogram = {}
+
+        for word in strs:
+
+            sorted_word = "".join(sorted(word))
+            
+
+            if sorted_word not in histogram:
+                histogram[sorted_word] = []
+            
+            histogram[sorted_word].append(word)
+
+    
+        return list(histogram.values())
